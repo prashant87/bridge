aa91bfaff8549091fa593338f58b5d80ce5ef1c9
z80 <bashkirov.sergey@gmail.com>
2018-10-24 07:54:06 -0700

    a

--- software/test_app/src/main.cpp
+++ software/test_app/src/main.cpp
@@ -1,8 +1,33 @@
 
 #include <iostream>
+#include <iomanip>
+#include "io.h"
 
 int main( int argc, char * argv[] )
 {
+    const int QTY = 5;
+    IoData data[QTY];
+    Io io;
+
+
+    for ( ;; )
+    {
+        const int qty = io.read( data, QTY );
+        if ( qty > 0 )
+        {
+            std::cout << "###############" << std::endl;
+            std::cout << "qty: qty" << std::endl;
+            for ( int i=0; i<qty; i++ )
+            {
+                const IoData & d = data[i];
+                for ( int j=0; j<7; j++ )
+                    std::cout << " " << std::setw( 5 ) << d.v[j];
+                std::cout << std::endl;
+            }
+        }
+        else
+
+    }
 
     return 0;
 }
