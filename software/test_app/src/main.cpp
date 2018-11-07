e67a1fe24b6606ec8764b4f3e3360ce4d1fd0d3c
sbashkirov <sergey.bashkirov@sony.com>
2018-11-07 15:18:46 -0800

    a

--- software/test_app/src/main.cpp
+++ software/test_app/src/main.cpp
@@ -13,6 +13,9 @@ int main( int argc, char * argv[] )
     Io io;
 
 
+    const int N = 10000;
+    int n = 0;
+    auto t_start = std::chrono::steady_clock::now();
     for ( ;; )
     {
         const int qty = io.read( data, QTY );
@@ -27,11 +30,24 @@ int main( int argc, char * argv[] )
                     std::cout << " " << std::setw( 5 ) << d.v[j];
                 std::cout << std::endl;
             }
+            n += qty;
         }
         else
         {
             // Sleep
-            std::this_thread::sleep_for( std::chrono::milliseconds(1) );
+            std::this_thread::sleep_for( std::chrono::milliseconds(30) );
+        }
+
+        if ( n > N )
+        {
+            const auto t_end = std::chrono::steady_clock::now();
+            std::chrono::duration<double, std::micro> duration = ( t_end - t_start );
+            double dt = static_cast<double>( duration.count() );
+            double nps = static_cast<double>( n ) / dt * 1.0e6;
+            std::cout << "readings per second: " << nps << std::endl;
+
+            n = 0;
+            t_start = t_end;
         }
     }
 
